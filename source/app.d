3ff86b22d5f3ae9a45ae1490b7ab705ed7c87c3f
fougas <fougas@localhost>
2018-03-29 18:02:58 +0500

    Implemented basic database query.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,10 @@
 import std.stdio;
 import sqlited;
+import std.typecons;
 
 void main() {
-	writeln(new Connection());
+	auto a = scoped!Database;
+	a.statement("create table test(x integer not null)");
+	a.statement("insert into test values(null)");
+	a.statement("drop table test");
 }
