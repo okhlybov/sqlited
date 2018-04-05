05205f0e00ba66548feb48005fa96aa497d51bc1
Oleg A. Khlybov <fougas@mail.ru>
2018-04-05 15:02:12 +0500

    Implemented parameter passing to the query.

--- source/app.d
+++ source/app.d
@@ -1,10 +1,16 @@
-import std.stdio;
 import sqlited;
+import std.stdio;
 import std.typecons;
 
 void main() {
 	auto a = scoped!Database;
-	a.statement("create table test(x integer not null)");
-	a.statement("insert into test values(null)");
-	a.statement("drop table test");
+	a.exec("create table test (x integer not null)");
+	a.exec("insert into test values(3)");
+	a.exec("insert into test values(1)");
+	a.exec("insert into test values(-1)");
+	a.exec("insert into test values(2)");
+    foreach(x; a.query("select * from test where x > ?", 1)) {
+        writeln(x[0]);
+    }
+	a.exec("drop table test");
 }
